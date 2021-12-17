3a3fea988b5c3a58fe0d89a78557aaeb0a9468e0
Kcisti <91692567+Kcisti@users.noreply.github.com>
2021-12-17 20:25:58 +0100

    v3

--- app.js
+++ app.js
@@ -11,8 +11,20 @@ getWebCam();
 var capture=document.getElementById("capture");
 var canvas=document.getElementById("canvas");
 var context=canvas.getContext('2d');
-
 capture.addEventListener("click",function(){
   document.getElementById('cam').style.display="none";
-    context.drawImage(video,0,0,650,490);
+    context.drawImage(video,0,0,900,1200);
 });
+function download(){
+	// Convert canvas to image
+	var dataURL = canvas.toDataURL("image/jpeg", 1.0);
+	downloadImage(dataURL, 'my-canvas.jpeg');
+// Save | Download image
+function downloadImage(data, filename = 'untitled.jpeg') {
+	var a = document.createElement('a');
+	a.href = data;
+	a.download = filename;
+	document.body.appendChild(a);
+	a.click();
+}
+}
